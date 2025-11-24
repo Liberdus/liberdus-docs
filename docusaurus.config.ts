7d81c99be473e88917824ebcf67088c555ad6394
soniasingla <soniasingla.1812@gmail.com>
2025-11-24 19:27:05 +0100

    deploy to main website

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   url: 'https://liberdus.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-test/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
